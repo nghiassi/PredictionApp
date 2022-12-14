dfc6bf2446276a77b2c74128fdebf1a59e42d965
nghiassi <106111786+nghiassi@users.noreply.github.com>
2022-12-14 00:04:52 -0800

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+pip install -U scikit-learn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
